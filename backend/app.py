54939aacd45d8f80e4ae4cb0e8fc700f91cdd528
Suryam saini <suryamsaini3@gmail.com>
2026-08-14 20:33:50 +0530

    Update backend and frontend

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
+from pymongo import MongoClient
 
 app = Flask(__name__)
-
 CORS(app)
 
+client = MongoClient("mongodb://127.0.0.1:27017/")
+db = client["classroom_engagement"]
+
+print("MongoDB connected successfully!")
 
 @app.route("/")
 def home():
